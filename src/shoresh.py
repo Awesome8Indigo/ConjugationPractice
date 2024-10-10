66453b8d1876c54ee8a1ef88ef682d0cbf8f7199
Indigo Fertig <iferti1629@inst.hcpss.org>
2024-10-10 18:58:09 +0000

    Add existing files

--- src/shoresh.py
+++ src/shoresh.py
@@ -3,13 +3,23 @@ class shoresh:
   def __init__(self, letters, meaning, *verbs):
     self.letters = letters
     self.verbs = verbs
-
+    self.jsonfile = open("src/conjugations.json", "rt")
+    self.jsontxt = self.jsonfile.read()
+    self.jsonlst = json.loads(self.jsontxt)
+    self.shrshlst = self.jsonlst["Shoresh"]
   def search(self) -> bool:
-    jsonfile = open("src/conjugations.json", "rt")
-    jsontxt = jsonfile.read()
-    jsonlst = json.loads(jsontxt)
-    shrshlst = jsonlst["Shoresh"]
-    #add a for loop to count for every shoresh in shrshlst
-    Letters = shrshlst[0]["Letters"]
-    if(self.letters == Letters):
-      return True
+ 
+    x = 0
+    # counts # of shoresh
+    for shrsh in self.shrshlst:
+      Letters = self.shrshlst[x]["Letters"]
+      if(self.letters == Letters):
+        return True
+      x += 1
+    
+    def addShoresh(self):
+      #using "search()" check if there is a search value.
+      if(self.search()):
+         return
+      
+    
