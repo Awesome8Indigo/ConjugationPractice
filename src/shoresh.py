269091d5b622ecc83e1cf3f0a2c70cc92d0982f6
Indigo Fertig <iferti1629@inst.hcpss.org>
2024-10-10 11:34:37 +0000

    Add existing files

--- src/shoresh.py
+++ src/shoresh.py
@@ -9,6 +9,7 @@ class shoresh:
     jsontxt = jsonfile.read()
     jsonlst = json.loads(jsontxt)
     shrshlst = jsonlst["Shoresh"]
+    #add a for loop to count for every shoresh in shrshlst
     Letters = shrshlst[0]["Letters"]
     if(self.letters == Letters):
       return True
