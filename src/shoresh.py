17725d6676e32610575e80c581325c94eecf28e6
Indigo Fertig <iferti1629@inst.hcpss.org>
2024-10-10 11:29:48 +0000

    Add existing file

--- src/shoresh.py
+++ src/shoresh.py
@@ -1,14 +1,14 @@
 import json
 class shoresh:
-  def __init__(letters, meaning, *verbs):
-    letters = letters
-    verbs = verbs
+  def __init__(self, letters, meaning, *verbs):
+    self.letters = letters
+    self.verbs = verbs
 
   def search(self) -> bool:
     jsonfile = open("src/conjugations.json", "rt")
     jsontxt = jsonfile.read()
     jsonlst = json.loads(jsontxt)
     shrshlst = jsonlst["Shoresh"]
-    for shrsh in shrshlst:
-      if(shrsh["Letters"] == shoresh):
-        return True
+    Letters = shrshlst[0]["Letters"]
+    if(self.letters == Letters):
+      return True
